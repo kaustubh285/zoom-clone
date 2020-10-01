e4a063349bac8dd6c90e8ca84584d8e571b9d432
kaustubh <kaustubh285@gmail.com>
2020-10-01 17:55:38 +0530

    used UUID to create new ids for each room and render the id in console using ejs file

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require("express");
+const { request } = require("http");
 
 const app = express();
+const { v4: uuidv4 } = require("uuid");
 
 const server = require("http").Server(app);
 
 app.set("view engine", "ejs");
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.render("room");
+  res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:room", (req, res) => {
+  res.render("room", { room_id: req.params.room });
 });
 
 server.listen(3030);

d32351b6741cbd3823cf75914311be2f33ac8e71
kaustubh <kaustubh285@gmail.com>
2020-10-02 17:44:30 +0530

    2 users showing their videos

--- server.js
+++ server.js
@@ -5,9 +5,15 @@ const { v4: uuidv4 } = require("uuid");
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
+
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
+app.use("/peerjs", peerServer);
 
 app.get("/", (req, res) => {
   res.redirect(`/${uuidv4()}`);
@@ -18,9 +24,9 @@ app.get("/:room", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-  socket.on("join-room", (roomId) => {
+  socket.on("join-room", (roomId, userId) => {
     socket.join(roomId);
-    socket.to(roomId).broadcast.emit("user-connected");
+    socket.to(roomId).broadcast.emit("user-connected", userId);
   });
 });
 
